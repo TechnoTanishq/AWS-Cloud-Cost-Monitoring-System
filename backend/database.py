bb65f80f854c4c35510c557658553924027abd1d
Mokshada Sheth <shethmokshada@gmail.com>
2026-03-16 15:34:58 +0530

    Created RDS connection

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DB_URL = "sqlite:///./finsight.db"
+load_dotenv()
+
+DB_URL = os.getenv("DATABASE_URL")
+
+if not DB_URL:
+    raise ValueError("DATABASE_URL not found in environment variables")
+
 engine = create_engine(DB_URL)
-session = sessionmaker(bind=engine, autoflush=False, autocommit = False)
-print("Database Connected") 
+
+session = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)
+
+print("✅ Database Connected")
